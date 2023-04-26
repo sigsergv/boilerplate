2004f6736e395190df116c2e94bad84d64b15113
Sergey Stolyarov <sergei@regolit.com>
2023-04-26 14:49:45 +0700

    Updated file

--- python/argparse-example.py
+++ python/argparse-example.py
@@ -7,6 +7,7 @@ parser = argparse.ArgumentParser(description='Process some files.')
 parser.add_argument('files', nargs='+', help='List of files to process')
 parser.add_argument('--action', '-a', dest='action_name', help='Action name')
 parser.add_argument('-d', dest='delete_after' help='Delete after processing.', action='store_true', default=False)
+parser.add_argumen('--cert-file', dest='cert_file', help='Certificate file, default: %(default)s', default='cert.pem')
 
 args = parser.parse_args()
 
